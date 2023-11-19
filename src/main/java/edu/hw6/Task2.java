7e0f28af50c01c7e9a8b656528ccd7b64b0e6e1b
Alexey Volkov <alexvolkovprogrammer@yandex.com>
2023-11-19 21:18:16 +0300

    firstVersionHW6

---
+++ src/main/java/edu/hw6/Task2.java
@@ -0,0 +1,19 @@
+package edu.hw6;
+
+/**
+ * При копировании файла в ту же папку в Проводнике Windows создается его копия,
+ * копия автоматически получает новое имя. Воспроизведём это поведение.
+ * Напишите функцию cloneFile(Path path), которая создает копию файла с новым именем.
+ * Например, файл называется Tinkoff Bank Biggest Secret.txt.
+ * Тогда новые имена файлов должны выглядеть следующим образом:
+ * Tinkoff Bank Biggest Secret.txt
+ * Tinkoff Bank Biggest Secret — копия.txt
+ * Tinkoff Bank Biggest Secret — копия (2).txt
+ * Tinkoff Bank Biggest Secret — копия (3).txt
+ */
+public class Task2 {
+    private Task2() {
+
+    }
+
+}
---
+++ src/main/java/edu/hw6/Task5.java
@@ -0,0 +1,7 @@
+package edu.hw6;
+
+public class Task5 {
+    private Task5() {
+
+    }
+}
